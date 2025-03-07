6ba512808b91146f0acca6dac41047ed75aeb51c
Prof. Rodrigo Franco <rodrigofrancos@gmail.com>
2025-03-07 07:56:16 -0300

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,10 @@
+## **2. Teste Unitário (`test_app.py`)**
+
+```python
+from app import app
+
+def test_home():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert response.data == b"Hello, CI/CD Pipeline!"
+```
